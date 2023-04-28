a2401c772ab6d342649e8c7865716d09e3c91cf1
kritikapatade <kritika.patade@utexas.edu>
2023-04-28 15:19:28 -0500

    Time and city inputs, csv file and dashed line

--- main.js
+++ main.js
@@ -5,10 +5,7 @@ window.addEventListener('DOMContentLoaded', function () {
         var scene = new BABYLON.Scene(engine);
         var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2,  Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
-        // camera.lowerRadiusLimit = camera.upperRadiusLimit = camera.radius = 1; // disables camera zoom
-
-        // var light = new BABYLON.HemisphericLight("hemiLight", new BABYLON.Vector3(-1, 1, 0), scene);
-        // light.diffuse = new BABYLON.Color3(1, 0, 0);
+        
         const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
 
         // Skybox
@@ -21,11 +18,6 @@ window.addEventListener('DOMContentLoaded', function () {
         skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
         skyboxMaterial.alpha = 0.90;
         skybox.material = skyboxMaterial;
-        // skybox.position = new BABYLON.Vector3(50,50,50);
-
-        // const sphereMaterial = new BABYLON.StandardMaterial("sphereMaterial", scene);
-        // sphereMaterial.diffuseColor = new BABYLON.StandardMaterial("sphereMaterial", scene);4
-        // sphereMaterial.alpha = 0.5;
 
         var sphere = new BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 5});
         const mat = new BABYLON.StandardMaterial("myMaterial", scene);
@@ -43,40 +35,129 @@ window.addEventListener('DOMContentLoaded', function () {
         console.log(x, y, z);
         sphere.position = new BABYLON.Vector3(x,y,z)
 
-        // const box = BABYLON.MeshBuilder.CreateBox("box", {height: 1, width: 0.75, depth: 0.25});
-        // const disc = BABYLON.MeshBuilder.CreateDisc("disc", {});
-        // const torus = BABYLON.MeshBuilder.CreateTorus("torus", {thickness: 0.1, diameter: 50, tessellation: 100});
-        // const myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
-        // myMaterial.diffuseColor = new BABYLON.Color3(1, 1, 0);
-        // myMaterial.alpha = 1;
-        // torus.material = myMaterial;
-        // torus.rotation = new BABYLON.Vector3(1.571,0,0);
-        // torus.position = new BABYLON.Vector3(0, -25, 0)
+        //const horizontal = BABYLON.MeshBuilder.CreateLines("lines", {points: [new BABYLON.Vector3(-50,0,0), new BABYLON.Vector3(50,0,0)]});
+        //const vertical = BABYLON.MeshBuilder.CreateLines("lines", {points: [new BABYLON.Vector3(0,0,-50), new BABYLON.Vector3(0,0,50)]});
+        //horizontal.color = new BABYLON.Color3(1,0,1);
+        //vertical.color = new BABYLON.Color3(1,0,1);
 
-        
-        // const points1 = [
-        //     new BABYLON.Vector3(0, 0, 0),
-        //     new BABYLON.Vector3(25, 0, 0)
-        // ]
-        // const line1 = BABYLON.MeshBuilder.CreateLines("lines", {points: points1});
-        // line1.color = new BABYLON.Color3(1, 0, 0);
-        // const points2 = [
-        //     new BABYLON.Vector3(0, 0, 0),
-        //     new BABYLON.Vector3(0, 25, 0)
-        // ]
-        // const line2 = BABYLON.MeshBuilder.CreateLines("lines", {points: points2});
-        // line2.color = new BABYLON.Color3(0, 1, 0);
-        // const points3 = [
-        //     new BABYLON.Vector3(0, 0, 0),
-        //     new BABYLON.Vector3(0, 0, 25)
-        // ]
-        // const line3 = BABYLON.MeshBuilder.CreateLines("lines", {points: points3});
-        // line3.color = new BABYLON.Color3(0, 0, 1);
 
-        const horizontal = BABYLON.MeshBuilder.CreateLines("lines", {points: [new BABYLON.Vector3(-50,0,0), new BABYLON.Vector3(50,0,0)]});
-        const vertical = BABYLON.MeshBuilder.CreateLines("lines", {points: [new BABYLON.Vector3(0,0,-50), new BABYLON.Vector3(0,0,50)]});
-        horizontal.color = new BABYLON.Color3(1,0,1);
-        vertical.color = new BABYLON.Color3(1,0,1);
+var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+	
+ 		//Input BABYLON.GUI
+                 const instPanel = new BABYLON.GUI.StackPanel();
+                 instPanel.width = "420px";
+                 instPanel.height = "200px";
+                 instPanel.paddingTop = "20px";
+                 instPanel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+                 instPanel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+                 instPanel.isVisible = true;
+                 advancedTexture.addControl(instPanel);
+                 var instTxt = new BABYLON.GUI.TextBlock();
+                 instTxt.height = "20px";
+                 instTxt.paddingTop = "5px";
+                 instTxt.color = "white";
+                 instTxt.text = "Moon Tracker";
+                 instTxt.fontSize = 20;
+                 instPanel.addControl(instTxt);
+              
+			
+ 				//Input BABYLON.GUI
+			
+                 const inputPanel = new BABYLON.GUI.StackPanel();
+                 inputPanel.width = "220px";
+                 inputPanel.height = "220px";
+                 inputPanel.paddingTop = "20px";
+                 inputPanel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+                 inputPanel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+                 inputPanel.isVisible = true;
+                 advancedTexture.addControl(inputPanel);
+			
+                 var city = new BABYLON.GUI.TextBlock();
+                 city.height = "20px";
+                 city.paddingTop = "5px";
+                 city.color = "white";
+                 city.text = "Enter City";
+                 city.fontSize = 15;
+                 inputPanel.addControl(city);
+			
+                 var cityInput = new BABYLON.GUI.InputText();
+                 cityInput.width = 0.5;
+                 cityInput.height = "30px";
+                 cityInput.paddingTop = "0px";
+                 cityInput.text = "Austin";
+                 cityName = "Austin";
+                 cityInput.color = "white";
+                 cityInput.background = "gray";
+              
+                 cityInput.onTextChangedObservable.add(e => {
+                     var sInput = parseInt(e.text);
+                     cityName = sInput;
+                  
+                 });
+                 inputPanel.addControl(cityInput);
+			
+ 				var time = new BABYLON.GUI.TextBlock();
+                 time.height = "20px";
+                 time.paddingTop = "5px";
+                 time.color = "white";
+                 time.text = "Enter Time";
+                 time.fontSize = 15;
+                 inputPanel.addControl(time);
+			
+                 var timeInput = new BABYLON.GUI.InputText();
+                 timeInput.width = 0.5;
+                 timeInput.height = "30px";
+                 timeInput.paddingTop = "0px";
+                 timeInput.text = "14:00";
+                 finalTime = "14:00";
+                 timeInput.color = "white";
+                 timeInput.background = "gray";
+              
+                 timeInput.onTextChangedObservable.add(e => {
+                     var sInput = parseInt(e.text);
+                     finalTime = sInput;
+                  
+                 });
+                 inputPanel.addControl(timeInput);
+			
+			
+ 				var playBtn = BABYLON.GUI.Button.CreateSimpleButton("but", "Show path");
+                 playBtn.width = 0.5;
+                 playBtn.height = "50px";
+                 playBtn.paddingTop = "20px";
+                 playBtn.color = "white";
+                 inputPanel.addControl(playBtn);
+                 playBtn.onPointerClickObservable.add(function (value) {
+                     //resetSimulation();
+                     console.log(cityName)
+                 });
+
+                
+                 let pnts = [new BABYLON.Vector3(-50,0,0),new BABYLON.Vector3(0,50,0), new BABYLON.Vector3(50,0,0)] // // parameterize with data fetch array of moon co-ordinates
+
+                 let dashedlines = BABYLON.MeshBuilder.CreateDashedLines("dashedlines",{points: pnts});
+
+                 const processCsv = () => {
+                    var fileName = "uscities.csv";
+                    BABYLON.Tools.LoadFile(fileName, (data) => {
+                        //console.log("here");
+                            const csv=data.split("\n");
+                            const result=csv.find((it, index) => {
+                                //console.log(index,it);
+                                const row=it.split(",");
+                                //console.log(row);
+                                if(row[0] === '"Austin"' ) // parameterize with cityName
+                                    return row;
+                                })
+                            //console.log(result.split(",")[6],result.split(",")[7]);
+                            //console.log(result);
+                            const lat=result.split(",")[6];
+                            const long=result.split(",")[7];
+                            console.log(lat,long);
+                        });
+                }
+                processCsv();
+
 
         return scene;
     }
@@ -85,4 +166,5 @@ window.addEventListener('DOMContentLoaded', function () {
     engine.runRenderLoop(function () {
         scene.render();
     });
+
 });
